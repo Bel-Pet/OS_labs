6f65f6f015631cb7b42bb7f1b3120217320a6d95
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-03-25 15:21:37 +0700

    first commit

---
+++ lab_9/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define ERROR_FORK -1
+#define ERROR 1
+#define SUCCESS 0
+
+int main() {
+    /* Создаем дочерний процесс
+     * В случае ошибки возвращается -1 */
+    pid_t child = fork();
+    if (child == ERROR_FORK) {
+        perror("fork()");
+        return ERROR;
+    }
+    /* В случае успеха родительскому процессу возвращается pid потомка,
+     * а потомку возвращается 0 */
+    if (child == SUCCESS) {
+    /* Замещяем тело дочернего процесса
+     * В случае ошибки замещения не произойдет и выполнится возврат с ошибкой */
+        execlp("cat", "cat", "text.txt", NULL);
+        perror("exec: cat");
+        _exit(ERROR);
+    }
+
+    /* Приостанавливаем процесс до выполнения дочернего процесса */
+    //wait(NULL);
+    printf("I am parent\n");
+
+    return SUCCESS;
+}
