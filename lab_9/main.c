f53eff0560623373227dd0cb41fea4d5b1cdd73c
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-04-25 19:04:10 +0700

    fix

--- lab_9/main.c
+++ lab_9/main.c
@@ -9,24 +9,16 @@
 #define SUCCESS 0
 
 int main() {
-    /* Создаем дочерний процесс
-     * В случае ошибки возвращается -1 */
     pid_t child = fork();
     if (child == ERROR_FORK) {
         perror("fork()");
         return ERROR;
     }
-    /* В случае успеха родительскому процессу возвращается pid потомка,
-     * а потомку возвращается 0 */
     if (child == SUCCESS) {
-    /* Замещяем тело дочернего процесса
-     * В случае ошибки замещения не произойдет и выполнится возврат с ошибкой */
         execlp("cat", "cat", "text.txt", NULL);
-        perror("exec: cat");
+        perror("exec");
         _exit(ERROR);
     }
-    /* Приостанавливаем процесс до выполнения дочернего процесса
-       В случае ошибки возвращается -1*/
     //pid_t res_wait = wait(NULL);
     //if (res_wait == ERROR_WAIT) {
     //    perror("wait()");
