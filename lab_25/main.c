99e8b0833a4c823ec648c046784f85ad6202575a
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-05-23 18:49:38 +0700

    add

--- lab_25/main.c
+++ lab_25/main.c
@@ -41,6 +41,13 @@ int transmitting_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
             perror("write to pipe");
             return ERROR;
         }
+
+        res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
+        if (res_close == CLOSE_ERROR) {
+            perror("close");
+            return ERROR;
+        }
+
         exit(SUCCESS);
     }
 
@@ -55,28 +62,36 @@ int receiving_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
     }
 
     if (res_fork == CHILD_CODE) {
-        int res_close = close(file_descriptors[1]);
+        int res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
         if (res_close == CLOSE_ERROR) {
             perror("close");
             return ERROR;
         }
 
         char buf[BUFFER_SIZE];
-        while (TRUE) {
-            int res_read = read(file_descriptors[0], buf, sizeof(buf));
-            if (res_read == READ_ERROR) {
-                perror("read");
-                return ERROR;
-            }
 
-            if (res_read == FILE_END) break;
-
-            int res_write = write(STDOUT_FILENO, buf, res_read);
-            if (res_write == WRITE_ERROR) {
-                perror("write");
-                return ERROR;
-            }
+        int res_read = read(file_descriptors[FIRST_DESCRIPTOR], buf, sizeof(buf));
+        if (res_read == READ_ERROR) {
+            perror("read");
+            return ERROR;
         }
+
+        for (int i = 0; i < res_read; i++){
+            buf[i] = toupper(buf[i]);
+        }
+
+        int res_write = write(STDOUT_FILENO, buf, res_read);
+        if (res_write == WRITE_ERROR) {
+            perror("write");
+            return ERROR;
+        }
+
+        res_close = close(file_descriptors[FIRST_DESCRIPTOR]);
+        if (res_close == CLOSE_ERROR) {
+            perror("close");
+            return ERROR;
+        }
+
         exit(SUCCESS);
     }
 
@@ -95,36 +110,11 @@ int main() {
     int res_transmitting_child = transmitting_child(file_descriptors);
     if (res_transmitting_child == ERROR) return ERROR;
 
-    int res_fork = fork();
-    if (res_fork == FORK_ERROR) {
-        perror("fork");
-        return ERROR;
-    }
+    int res_receiving_child =  receiving_child(file_descriptors);
+    if (res_receiving_child == ERROR) return ERROR;
 
-    if (res_fork == CHILD_CODE) {
-        int res_close = close(file_descriptors[1]);
-        if (res_close == EOF) {
-            perror("close");
-            return ERROR;
-        }
-        //printf("child 2: reading from pipe and uppercasing\n");
-        char buf[BUFFER_SIZE];
-        int r;
-        while ((r = read(file_descriptors[0], buf, sizeof(buf))) > 0) {
-            for (int i = 0; i < r; i++) {
-                buf[i] = toupper(buf[i]);
-            }
-            write(1, buf, r);
-        }
-        if (r == -1) {
-            perror("read from pipe");
-            return 1;
-        }
-        return SUCCESS;
-    }
+    close(file_descriptors[FIRST_DESCRIPTOR]);
+    close(file_descriptors[SECOND_DESCRIPTOR]);
 
-    close(file_descriptors[0]);
-    close(file_descriptors[1]);
-  
     return SUCCESS;
 }
