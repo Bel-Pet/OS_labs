0700b90e793eb78ab2bf2f77128710ced959695a
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-05-30 14:17:18 +0700

    fix

--- lab_25/main.c
+++ lab_25/main.c
@@ -5,54 +5,69 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-#define BUFFER_SIZE 256
-#define SUCCESS 0
-#define ERROR 1
 #define PIPE_ERROR -1
 #define FORK_ERROR -1
-#define CHILD_CODE 0
-#define NUMBER_OF_DESCRIPTORS 2
-#define FIRST_DESCRIPTOR 0
-#define SECOND_DESCRIPTOR 1
 #define CLOSE_ERROR -1
 #define WRITE_ERROR -1
 #define READ_ERROR -1
-#define END_READ 0
+#define WAIT_ERROR -1
 
-int sending_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
+#define END_READ 0
+#define CHILD_CODE 0
+
+#define NUMBER_OF_DESCRIPTORS 2
+#define FIRST_DESCRIPTOR 0
+#define SECOND_DESCRIPTOR 1
+
+#define SUCCESS 0
+#define ERROR 1
+
+#define BUFFER_SIZE 256
+
+int transfer_text(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
+    char buf[BUFFER_SIZE];
+    int res_read = 0;
+
+    do {
+        res_read = read(STDIN_FILENO, buf, BUFFER_SIZE);
+        if (res_read == READ_ERROR) {
+            perror("read");
+            return ERROR;
+        }
+
+        int res_write = write(file_descriptors[SECOND_DESCRIPTOR], buf, res_read);
+        if (res_write == WRITE_ERROR) {
+            perror("write to pipe");
+            return ERROR;
+        }
+    } while (res_read > END_READ);
+
+    return SUCCESS;
+}
+
+int create_and_start_sending_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
     int res_fork = fork();
     if (res_fork == FORK_ERROR) {
         perror("fork");
-
         return ERROR;
     }
-    /// родительский процесс завершает свою работу в функции после удачного создания потомка
-    if (res_fork > CHILD_CODE) return res_fork;
 
-    /// закрываем конец отвечающего за запись в программный канала
+    /// родительский процесс завершает свою работу в функции после удачного создания потомка
+    if (res_fork > CHILD_CODE) return SUCCESS;
+
+    /// закрываем конец отвечающий за запись в программный канала
     int res_close = close(file_descriptors[FIRST_DESCRIPTOR]);
     if (res_close == CLOSE_ERROR) {
         perror("close");
         exit(ERROR);
     }
 
-    char buf[BUFFER_SIZE];
-    int res_read = 0;
-
     /// считываем текст и передаем его через программный канала
-    do {
-        res_read = read(STDIN_FILENO, buf, BUFFER_SIZE);
-        if (res_read == READ_ERROR) {
-            perror("read");
-            exit(ERROR);
-        }
-
-        int res_write = write(file_descriptors[SECOND_DESCRIPTOR], buf, res_read);
-        if (res_write == WRITE_ERROR) {
-            perror("write to pipe");
-            exit(ERROR);
-        }
-    } while (res_read > END_READ);
+    int res_transfer = transfer_text(file_descriptors);
+    if (res_transfer == ERROR) {
+        close(file_descriptors[SECOND_DESCRIPTOR]);
+        exit(ERROR);
+    }
 
     /// закрываем оставшийся конец канала
     res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
@@ -64,32 +79,15 @@ int sending_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
     exit(SUCCESS);
 }
 
-int receiving_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
-    int res_fork = fork();
-    if (res_fork == FORK_ERROR) {
-        perror("fork");
-        return ERROR;
-    }
-
-    /// родительский процесс завершает свою работу в функции после удачного создания потомка
-    if (res_fork > CHILD_CODE) return SUCCESS;
-
-    /// закрываем конец отвечающего за запись в программный канала
-    int res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
-    if (res_close == CLOSE_ERROR) {
-        perror("close");
-        return ERROR;
-    }
-
+int convert_text(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
     char buf[BUFFER_SIZE];
     int res_read = 0;
 
-    /// считываем и преобразуем текст, затем выводим его
     do {
         res_read = read(file_descriptors[FIRST_DESCRIPTOR], buf, sizeof(buf));
         if (res_read == READ_ERROR) {
             perror("read to pipe");
-            exit(ERROR);
+            return ERROR;
         }
 
         for (int i = 0; i < res_read; i++){
@@ -99,9 +97,36 @@ int receiving_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
         int res_write = write(STDOUT_FILENO, buf, res_read);
         if (res_write == WRITE_ERROR) {
             perror("write");
-            exit(ERROR);
+            return ERROR;
         }
     } while (res_read > END_READ);
+
+    return SUCCESS;
+}
+
+int create_and_start_receiving_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
+    int res_fork = fork();
+    if (res_fork == FORK_ERROR) {
+        perror("fork");
+        return ERROR;
+    }
+
+    /// родительский процесс завершает свою работу в функции после удачного создания потомка
+    if (res_fork > CHILD_CODE) return SUCCESS;
+
+    /// закрываем конец отвечающий за запись в программный канала
+    int res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
+    if (res_close == CLOSE_ERROR) {
+        perror("close");
+        exit(ERROR);
+    }
+
+    /// считываем и преобразуем текст, затем выводим его
+    int res_convert = convert_text(file_descriptors);
+    if (res_convert == ERROR) {
+        close(file_descriptors[FIRST_DESCRIPTOR]);
+        exit(ERROR);
+    }
 
     /// закрываем оставшийся конец канала
     res_close = close(file_descriptors[FIRST_DESCRIPTOR]);
@@ -113,33 +138,50 @@ int receiving_child(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
     exit(SUCCESS);
 }
 
-int main() {
-    int file_descriptors[NUMBER_OF_DESCRIPTORS];
+int close_descriptors(int file_descriptors[NUMBER_OF_DESCRIPTORS]) {
+    int res_close = close(file_descriptors[FIRST_DESCRIPTOR]);
+    if (res_close == CLOSE_ERROR) {
+        perror("close");
+        return ERROR;
+    }
+    res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
+    if (res_close == CLOSE_ERROR) {
+        perror("close");
+        return ERROR;
+    }
+    return SUCCESS;
+}
 
+int main() {
+    /// создаем массив для заполнения его концами межпроцессорного канала
+    int file_descriptors[NUMBER_OF_DESCRIPTORS];
+    /// создаем межпроцессорный канал
     int res_pipe = pipe(file_descriptors);
     if (res_pipe == PIPE_ERROR) {
         perror("pipe");
         return ERROR;
     }
 
-    int res_sending_child = sending_child(file_descriptors);
-    if (res_sending_child == ERROR) return ERROR;
-
-    int res_receiving_child =  receiving_child(file_descriptors);
-    if (res_receiving_child == ERROR) return ERROR;
-
-    int res_close = close(file_descriptors[FIRST_DESCRIPTOR]);
-    if (res_close == CLOSE_ERROR) {
-        perror("close");
+    /// создаем дочерний процесс, передающий текс по межпроцессорному каналу
+    int res_sending_child = create_and_start_sending_child(file_descriptors);
+    if (res_sending_child == ERROR) {
+        close_descriptors(file_descriptors);
+        return ERROR;
+    }
+    /// создаем дочерний процесс, принимающий текс по межпроцессорному каналу
+    int res_receiving_child =  create_and_start_receiving_child(file_descriptors);
+    if (res_receiving_child == ERROR) {
+        close_descriptors(file_descriptors);
         return ERROR;
     }
 
-    res_close = close(file_descriptors[SECOND_DESCRIPTOR]);
-    if (res_close == CLOSE_ERROR) {
-        perror("close");
-        return ERROR;
-    }
-    wait(NULL);
+    /// закрываем дескрипторы
+    int res_close = close_descriptors(file_descriptors);
+    if (res_close == ERROR) return ERROR;
+
+    /// ожидаем завершения дочернего процесса
+    int res_wait = wait(NULL);
+    if (res_wait == WAIT_ERROR) return ERROR;
 
     return SUCCESS;
 }
